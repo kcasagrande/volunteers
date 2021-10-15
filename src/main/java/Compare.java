d731469b02b13fcfcc3e9ec8b1adb1868e11c2c8
Kaunogan <kaunogan.lemoine@sparte.io>
2021-10-15 15:12:04 +0200

    jean loui commit le retout (fix bug)

---
+++ src/main/java/Compare.java
@@ -0,0 +1,15 @@
+public class Compare {
+
+  public Volonteer[] volonteers = new Volonteer[900];
+  public
+
+  public Compare(Volonteer[] newVolonteers) {
+      this.volonteers = newVolonteers;
+  }
+  public static Volonteer[] CompareNum(String volonteerNum){
+    for(int index = 0; inder < volonteers.size(); index ++ ){
+
+      if (volonteerNum == volonteers[index].tel)
+    }
+  }
+}
