649862d8308928b6d546092a0f0741c29ee8e776
Alexandre <alexandre.gelin71@gmail.com>
2021-10-15 11:42:40 +0200

    create usermock

---
+++ src/main/java/org/example/volunteers/UserMock.java
@@ -0,0 +1,13 @@
+package org.example.volunteers;
+
+public class UserMock {
+
+    private final String lastName ;
+    private final String firstName ;
+
+    public UserMock(String lastName, String firstName){
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+}
