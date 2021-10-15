68e21a639e2eb44e50d0b4a5d5e574a7c3d8efab
Kaunogan <kaunogan.lemoine@sparte.io>
2021-10-15 15:55:49 +0200

    add CompareFirstNameByName

--- src/main/java/Compare.java
+++ src/main/java/Compare.java
@@ -8,7 +8,7 @@ public class Compare {
   }
 
   public Volonteer[] CompareNum(String volonteerNum){
-    
+
     for(int index = 0; index < volonteers.length ; index ++ ){
       if (volonteerNum == volonteers[index].tel){
         this.comparedVolonteers[index]= volonteers[index];
@@ -18,37 +18,57 @@ public class Compare {
     return this.comparedVolonteers;
   }
 
-  public Volonteer[] CompareMail(String volonteerMail){
-    
+  public Volonteer[] CompareMail(Volonteer comparedVolonteers, String volonteerMail){
+
     for(int index = 0; index < volonteers.length ; index++ ){
       if (volonteerMail == volonteers[index].mail){
-        this.comparedVolonteers[index]= volonteers[index];
+        comparedVolonteers[index]= volonteers[index];
+      }
+
+    }
+    return comparedVolonteers;
+  }
+
+  public Volonteer[] CompareFirstName(Volonteer comparedVolonteers,String volonteerFirstName){
+
+    for(int index = 0; index < comparedVolonteers.length ; index++ ){
+      if (volonteerFirstName !== comparedVolonteers[index].firstname){
+        comparedVolonteers[index]= volonteers[index];
+      }
+
+    }
+    return comparedVolonteers;
+  }
+
+  public Volonteer[] CompareName(Volonteer comparedVolonteers, String volonteerName){
+
+    for(int index = 0; index < comparedVolonteers.length ; index++ ){
+      if (volonteerName !== comparedVolonteers[index].name){
+        comparedVolonteers[index]= volonteers[index];
+      }
+
+    }
+    return comparedVolonteers;
+  }
+
+  public Volonteer[] CompareFirstNameByName(Volonteer comparedVolonteers, String volonteerName){
+
+    for(int index = 0; index < comparedVolonteers.length ; index++ ){
+      if (volonteerName !== comparedVolonteers[index].firstname){
+        comparedVolonteers[index]= volonteers[index];
       }
 
     }
     return this.comparedVolonteers;
   }
+  public Volonteer[] CompareNameByFirstName(Volonteer comparedVolonteers, String volonteerFirstName){
 
-  public Volonteer[] CompareFirstName(String volonteerFirstName){
-    
-    for(int index = 0; index < volonteers.length ; index++ ){
-      if (volonteerFirstName == volonteers[index].firstname){
-        this.comparedVolonteers[index]= volonteers[index];
+    for(int index = 0; index < comparedVolonteers.length ; index++ ){
+      if (volonteerFirstName !== comparedVolonteers[index].name){
+        comparedVolonteers[index]= volonteers[index];
       }
 
     }
-    return this.comparedVolonteers;
+    return comparedVolonteers;
   }
-
-  public Volonteer[] CompareName(String volonteerName){
-    
-    for(int index = 0; index < volonteers.length ; index++ ){
-      if (volonteerName == volonteers[index].name){
-        this.comparedVolonteers[index]= volonteers[index];
-      }
-
-    }
-    return this.comparedVolonteers;
-  }
-
 }
