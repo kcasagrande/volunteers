e347199146bc03aad6ba2346c2cbec2a30870287
Léo GIGANT <rowix4@gmail.com>
2021-10-15 17:11:13 +0200

    Added process to sort clients list and remove duplicates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,8 +8,9 @@ public class App {
     public static void main(String[] args) throws IOException {
         CsvReader csvReader = new CsvReader();
         List<String[]> fileLines = csvReader.getLinesFromFile("src/main/resources/data.csv");
-        
+
         ClientList clientList = new ClientList();
         clientList.setClientListFromFileLines(fileLines);
+        clientList.normalize();
     }
 }
--- src/main/java/org/example/volunteers/models/ClientList.java
+++ src/main/java/org/example/volunteers/models/ClientList.java
@@ -1,7 +1,9 @@
 package org.example.volunteers.models;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 public class ClientList {
     private ArrayList<Client> clientList = new ArrayList<Client>();
@@ -19,11 +21,99 @@ public class ClientList {
         return clientList;
     }
 
-    public ArrayList<Client> getClientListClean() {
-        return clientListClean;
-    }
+    public ArrayList<Client> getClientListClean() { return clientListClean; }
 
     public void normalize() {
-        
+        int i = 0;
+        for(Client client : clientList) {
+            ArrayList<Client> duplicatesClients = new ArrayList<Client>();
+            duplicatesClients.add(client);
+
+            int y = i;
+            for (Client clientPotentialDuplicate : clientList) {
+                if (y <= i) { continue; }
+
+                if (this.isDuplicate(client, clientPotentialDuplicate)) {
+                    duplicatesClients.add(clientPotentialDuplicate);
+
+                    clientList.remove(y);
+                }
+
+                y++;
+            }
+
+            clientListClean.add(this.mergeDuplicates(duplicatesClients));
+
+            i++;
+        }
+    }
+
+    private boolean isDuplicate(Client clientA, Client clientB) {
+        // TODO: format string chars
+        boolean firstNameMatch = Objects.equals(clientA.getFirstname(), clientB.getFirstname());
+        boolean lastNameMatch = Objects.equals(clientA.getLastname(), clientB.getLastname());
+        boolean usernameMatch = Objects.equals(clientA.getUsername(), clientB.getUsername());
+        boolean emailMatch = Objects.equals(clientA.getEmail(), clientB.getEmail());
+        boolean phoneMatch = Objects.equals(clientA.getPhone(), clientB.getPhone());
+
+        if (usernameMatch || emailMatch || phoneMatch) {
+            return true;
+        }
+
+        return firstNameMatch && lastNameMatch;
+    }
+
+    private Client mergeDuplicates(ArrayList<Client> duplicatesClients) {
+        if (duplicatesClients.size() == 1) {
+            return duplicatesClients.get(0);
+        }
+
+        String firstName = this.getUniqueForAttribute("firstname", duplicatesClients);
+        String lastName = this.getUniqueForAttribute("lastname", duplicatesClients);
+        String username = this.getUniqueForAttribute("username", duplicatesClients);
+        String email = this.getUniqueForAttribute("email", duplicatesClients);
+        String phone = this.getUniqueForAttribute("phone", duplicatesClients);
+
+        return new Client(firstName, lastName, username, email, phone);
+    }
+
+    private String getUniqueForAttribute(String attribute, ArrayList<Client> duplicatesClients) {
+        HashMap<String, Integer> attributes = new HashMap<String, Integer>();
+
+        for (Client client: duplicatesClients) {
+            switch (attribute) {
+                case "firstname":
+                    attribute = client.getFirstname();
+                    break;
+                case "lastname":
+                    attribute = client.getLastname();
+                    break;
+                case "username":
+                    attribute = client.getUsername();
+                    break;
+                case "email":
+                    attribute = client.getEmail();
+                    break;
+                case "phone":
+                    attribute = client.getPhone();
+                    break;
+                default:
+                    break;
+            }
+
+            if (attributes.containsKey(attribute)) {
+                attributes.put(attribute, 0);
+            } else {
+                attributes.put(attribute, attributes.get(attribute) + 1);
+            }
+        }
+
+        return this.getMostFromStringList(attributes);
+    }
+
+    private String getMostFromStringList(HashMap<String, Integer> strings) {
+        // TODO
+
+        return "";
     }
 }
