6b7020d3968fd73301de9db3a4b627a8eb2670fe
Nibaldo <ndonoso@witivio.com>
2021-10-15 15:43:50 +0200

    rename test to ContactTest

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -17,39 +16,9 @@ public class App {
             .stream().map(string -> string.split(";"))
             .collect(toList());
 
-        // Apply dark magic here...
-        LinkedList<String> contactList = new LinkedList<String>();
-        contactList.add("FirstName");
-        contactList.add("LastName");
-        contactList.add("NickName");
-        contactList.add("Email");
-        contactList.add("Phone");
+        List<Contact> contacts = CreateContactList(personnes);
+        System.out.println(contacts.get(0).firstName);
 
-        List<Contact> contacts = new ArrayList<Contact>();
-
-        try {
-            for (String[] _contact : personnes) {
-                ArrayList list = new ArrayList<>(Arrays.asList(_contact));
-                if (list.size() == contactList.size()){
-                    contacts.add(new Contact(){{
-                        firstName = _contact[contactList.indexOf("FirstName")].trim();
-                        lastName = _contact[contactList.indexOf("LastName")].trim();
-                        nickName = _contact[contactList.indexOf("NickName")].trim();
-                        email = _contact[contactList.indexOf("Email")].trim();
-                        phone = _contact[contactList.indexOf("Phone")].trim();
-                    }});
-                }
-            }
-
-            for (Contact c : contacts){
-                if(!isEmailValid(c.email)){
-
-                }
-            }
-
-        }catch (Exception ex){
-
-        }
     }
 
     public static boolean isEmailValid(String email){
@@ -60,5 +29,32 @@ public class App {
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
-    
+
+    public static List<Contact> CreateContactList(List<String[]> contactLine) {
+
+        // Apply dark magic here...
+        LinkedList<String> contactList = new LinkedList<String>();
+        contactList.add("FirstName");
+        contactList.add("LastName");
+        contactList.add("NickName");
+        contactList.add("Email");
+        contactList.add("Phone");
+
+        List<Contact> contacts = new ArrayList<Contact>();
+
+        for (String[] _contact : contactLine) {
+            ArrayList listContact = new ArrayList<>(Arrays.asList(_contact));
+            if (listContact.size() == contactList.size()){
+                Contact newContact = new Contact(){{
+                    firstName = _contact[contactList.indexOf("FirstName")].trim();
+                    lastName = _contact[contactList.indexOf("LastName")].trim();
+                    nickName = _contact[contactList.indexOf("NickName")].trim();
+                    email = _contact[contactList.indexOf("Email")].trim();
+                    phone = _contact[contactList.indexOf("Phone")].trim();
+                }};
+                contacts.add(newContact);
+            }
+        }
+        return contacts;
+    }
 }
---
+++ src/test/java/ContactTest.java
@@ -0,0 +1,69 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ContactTest {
+
+    private Function<String, Integer> dependency;
+    List<Contact> contacts_test = new ArrayList<Contact>();
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        dependency = (String string) -> 1;
+        contacts_test = App.CreateContactList(Files.readAllLines(Paths.get("src/main/resources/data.csv"))
+               .stream().map(string -> string.split(";"))
+               .collect(toList()));
+    }
+
+    @Test
+    public void checkFirstPerson() {
+        // Arrange
+        String lastNameToCheck = contacts_test.get(0).firstName;
+        // Act
+
+
+        // Assert
+        assertEquals("Rébecca", lastNameToCheck, "Ce message s'affiche si le test échoue");
+    }
+
+    @Test
+    public void checkIfEmailIsCorrect() {
+        // Arrange
+        String emailToCheck = contacts_test.get(0).email;
+        // Act
+
+
+        // Assert
+        assertEquals(true, App.isEmailValid(emailToCheck), "Ce message s'affiche si le test échoue");
+    }
+
+    @Test
+    public void checkIsNameInEmail(){
+        // Arrange
+        String email = contacts_test.get(0).email;
+        String firstName = contacts_test.get(0).firstName;
+
+        assertEquals(true, email.contains(firstName), "Le firstName n'est pas dans le email");
+    }
+
+    @Test
+    public void checkIfPhoneIsCorrect() {
+        // Arrange
+        String phoneToCheck = contacts_test.get(0).phone;
+        // Act
+
+
+        // Assert
+        assertEquals(true, Contact.checkIsValidNumberPhone(phoneToCheck), "Ce message s'affiche si le test échoue");
+    }
+
+}
