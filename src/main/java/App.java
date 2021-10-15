0df16359463c2a8509a595b03dfd9d719596dc15
TheeeoooS <theo.segard@ynov.com>
2021-10-15 16:15:34 +0200

    maj App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import Services.csv.CsvService;
 import exceptions.CsvEmptyException;
 import exceptions.CsvNotExistException;
+import user.User;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,7 +14,8 @@ public class App {
     public static void main(String[] args) throws IOException, CsvNotExistException, CsvEmptyException {
         CsvService csvService = new CsvService("src/main/resources/data.csv");
         List<String[]> lines = csvService.readAllLines();
-        
+        List<User> linesUser = csvService.convertListUser(lines);
+
         System.out.println("Result goes here");
     }
 }
