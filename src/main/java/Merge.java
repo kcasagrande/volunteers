ffb076f7b5db8a0bf49c486b2cc86b1c5edf0df6
GUMBAUElric <elric.gumbau@ynov.com>
2021-10-15 19:54:55 +0200

    fix(merge): fix syntax errors

--- src/main/java/Merge.java
+++ src/main/java/Merge.java
@@ -1,9 +1,11 @@
+import java.util.List;
+
 public class Merge{
 
-  public static Volonteer MergeName(List<Volonteer> volonteers){
-    if(!volonteers.get(0).name){
+  public static List<Volonteer> MergeName(List<Volonteer> volonteers){
+    if(volonteers.get(0).name == ""){
       for(int i =0; i<volonteers.size(); i++){
-        if(volonteers.get(i).name && volonteers.get(i).name != volonteers.get(0).firstname){
+        if(volonteers.get(i).name != "" && volonteers.get(i).name != volonteers.get(0).firstname){
           volonteers.get(0).name = volonteers.get(i).name;
           break;
         }
@@ -12,11 +14,11 @@ public class Merge{
     return volonteers;
   }
 
-  public static Volonteer MergeFirstName(List<Volonteer> volonteers){
-    if(!volonteers.get(0).firstName){
+  public static List<Volonteer> Mergefirstname(List<Volonteer> volonteers){
+    if(volonteers.get(0).firstname == ""){
       for(int i =0; i<volonteers.size(); i++){
-        if(volonteers.get(i).firstName && volonteers.get(i).firstName != volonteers.get(0).Name){
-          volonteers.get(0).firstName = volonteers.get(i).firstName;
+        if(volonteers.get(i).firstname != "" && volonteers.get(i).firstname != volonteers.get(0).name){
+          volonteers.get(0).firstname = volonteers.get(i).firstname;
           break;
         }
       }
@@ -24,10 +26,10 @@ public class Merge{
     return volonteers;
   }
 
-  public static Volonteer MergeNametag(List<Volonteer> volonteers){
-    if(!volonteers.get(0).nametag){
+  public static List<Volonteer> MergeNametag(List<Volonteer> volonteers){
+    if(volonteers.get(0).nametag == ""){
       for(int i =0; i<volonteers.size(); i++){
-        if(volonteers.get(i).nametag){
+        if(volonteers.get(i).nametag != ""){
           volonteers.get(0).nametag = volonteers.get(i).nametag;
           break;
         }
@@ -36,10 +38,10 @@ public class Merge{
     return volonteers;
   }
 
-  public static Volonteer MergeMail(List<Volonteer> volonteers){
-    if(!volonteers.get(0).mail){
+  public static List<Volonteer> MergeMail(List<Volonteer> volonteers){
+    if(volonteers.get(0).mail == ""){
       for(int i =0; i<volonteers.size(); i++){
-        if(volonteers.get(i).mail){
+        if(volonteers.get(i).mail != ""){
           volonteers.get(0).mail = volonteers.get(i).mail;
           break;
         }
@@ -48,10 +50,10 @@ public class Merge{
     return volonteers;
   }
 
-  public static Volonteer MergeTel(List<Volonteer> volonteers){
-    if(!volonteers.get(0).tel){
+  public static List<Volonteer> MergeTel(List<Volonteer> volonteers){
+    if(volonteers.get(0).tel == ""){
       for(int i =0; i<volonteers.size(); i++){
-        if(volonteers.get(i).tel){
+        if(volonteers.get(i).tel != ""){
           volonteers.get(0).tel = volonteers.get(i).tel;
           break;
         }
