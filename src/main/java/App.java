6069189576f202934c21ebe07512b8e86ae94c96
dherve <dherve@iidre.com>
2021-10-15 11:58:06 +0200

    lines was sorted

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,10 +8,13 @@ import static java.util.stream.Collectors.toList;
 public class App {
     public static void main(String[] args) throws IOException {
         List<String[]> lines = Files.readAllLines(Paths.get("src/main/resources/data.csv"))
-            .stream().map(string -> string.split(";"))
+            .stream()
+            .sorted()
+            .map(string -> string.split(";"))
             .collect(toList());
 
         // Apply dark magic here...
+        lines.stream().map(line -> String.join(";", line)).forEach(System.out::println);
 
         System.out.println("Result goes here");
     }
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+import org.example.volunteers.Demo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+}
