fac4ed5ac2941981d1b6180ce7cd55d36dcff16e
Anthony Dubuis <anthony@MacBook-Air-de-Anthony.local>
2021-10-15 12:50:41 +0200

    envoie de la liste de Person avec tous les doublons

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import model.Person;
 import model.PersonProperties;
 import service.Parser;
+import service.sortList.GetAllPerson;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,27 +11,17 @@ import java.util.Map;
 
 public class App {
     public static Parser parser = new Parser();
+    public static GetAllPerson getAllPerson = new GetAllPerson();
 
 
     public static void main(String[] args) throws IOException {
          List<Map<PersonProperties, String>> lol = parser.parseCsv("src/main/resources/data.csv",";");
          System.out.println(lol);
 
-        List<Person> listPersonWDuplicate = new ArrayList<>();
 
-        for ( int i = 0; i < lol.size(); i++){
-            Person person = new Person();
+        List<Person> listPerson = getAllPerson.getListPersonWDuplicate(lol);
+        System.out.println(listPerson);
 
-            person.id = i +1;
-            person.lastName = lol.get(i).get(PersonProperties.lastName);
-            person.firstName = lol.get(i).get(PersonProperties.firstName);
-            person.userName = lol.get(i).get(PersonProperties.userName);
-            person.email = lol.get(i).get(PersonProperties.email);
-            person.phoneNumber = lol.get(i).get(PersonProperties.phoneNumber);
-
-            listPersonWDuplicate.add(person);
-        }
-System.out.println(listPersonWDuplicate);
         // Apply dark magic here...
 
         System.out.println("Result goes here");
