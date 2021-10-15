2761560eee1da13de93f14a3a14860d76f0e8a8f
dherve <dherve@iidre.com>
2021-10-15 15:37:32 +0200

    Test grouped done.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,20 +2,38 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.function.Function;
 
 import static java.util.stream.Collectors.toList;
 
 public class App {
+
+    public static Function<String, String[]> splitCSV = (line) -> line.split(";");
+    public static Function<String[], String> joinCSV = (line) -> String.join(";", line);
+
     public static void main(String[] args) throws IOException {
         List<String[]> lines = Files.readAllLines(Paths.get("src/main/resources/data.csv"))
             .stream()
             .sorted()
-            .map(string -> string.split(";"))
+            .map(splitCSV)
             .collect(toList());
 
         // Apply dark magic here...
-        lines.stream().map(line -> String.join(";", line)).forEach(System.out::println);
-
-        System.out.println("Result goes here");
+        lines.stream().map(joinCSV).forEach(System.out::println);
     }
+
+    public static List<String[]> groupedInput(List<String[]> actualLines) {
+        return actualLines
+                .stream()
+                .map(joinCSV)
+                .sorted(String.CASE_INSENSITIVE_ORDER)
+                .map(splitCSV)
+                .collect(toList());
+    }
+
+//    public List<String[]> fusedInput(List<String[]> actualLines) {
+//        return actualLines
+//                .stream()
+//                .ma
+//    }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,10 +2,114 @@ import org.example.volunteers.Demo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
+
+    private App app;
+
+    public void testActualAndExpected(List<String[]> actualList, List<String[]> expectedList){
+        for(int i=0; i < actualList.size(); i++){
+            for(int j=0; j < actualList.get(i).length; j++){
+                assertEquals(actualList.get(i)[j], expectedList.get(i)[j]);
+            }
+        }
+    }
+
+    @BeforeEach
+    public void setUp() {
+        app = new App();
+    }
+
+    @Test
+    public void entriesMustBeGroupedByLetter() {
+        List<String[]> actualLines = new ArrayList<>();
+        actualLines.add(new String[]{"B", "", "", "", ""});
+        actualLines.add(new String[]{"A", "", "", "", ""});
+        actualLines.add(new String[]{"b", "", "", "", ""});
+        actualLines.add(new String[]{"a", "", "", "", ""});
+
+        List<String[]> expectedLines = new ArrayList<>();
+        expectedLines.add(new String[]{"A", "", "", "", ""});
+        expectedLines.add(new String[]{"a", "", "", "", ""});
+        expectedLines.add(new String[]{"B", "", "", "", ""});
+        expectedLines.add(new String[]{"b", "", "", "", ""});
+
+        actualLines = this.app.groupedInput(actualLines);
+
+        this.testActualAndExpected(actualLines, expectedLines);
+
+    }
+
+    @Test
+    public void entriesMustBeGroupedByLetterByNumber() {
+        List<String[]> actualLines = new ArrayList<>();
+        actualLines.add(new String[]{"A", "", "", "", "1"});
+        actualLines.add(new String[]{"Aa", "", "", "", "1"});
+        actualLines.add(new String[]{"a", "", "", "", "1"});
+        actualLines.add(new String[]{"aA", "", "", "", "2"});
+        actualLines.add(new String[]{"a", "", "", "", "2"});
+
+
+        List<String[]> expectedLines = new ArrayList<>();
+        expectedLines.add(new String[]{"A", "", "", "", "1"});
+        expectedLines.add(new String[]{"a", "", "", "", "1"});
+        expectedLines.add(new String[]{"a", "", "", "", "2"});
+        expectedLines.add(new String[]{"Aa", "", "", "", "1"});
+        expectedLines.add(new String[]{"aA", "", "", "", "2"});
+
+
+        actualLines = this.app.groupedInput(actualLines);
+
+        this.testActualAndExpected(actualLines, expectedLines);
+
+    }
+
+    @Test
+    public void entriesMustBeGroupedAll() {
+        List<String[]> actualLines = new ArrayList<>();
+        actualLines.add(new String[]{"b", "", "", "", "1"});
+        actualLines.add(new String[]{"A", "", "", "", "1"});
+        actualLines.add(new String[]{"cC", "", "", "", "2"});
+        actualLines.add(new String[]{"a", "", "", "", "1"});
+        actualLines.add(new String[]{"b", "", "", "", "2"});
+        actualLines.add(new String[]{"c", "", "", "", "2"});
+        actualLines.add(new String[]{"Cc", "", "", "", "2"});
+        actualLines.add(new String[]{"B", "", "", "", "2"});
+
+        List<String[]> expectedLines = new ArrayList<>();
+        expectedLines.add(new String[]{"A", "", "", "", "1"});
+        expectedLines.add(new String[]{"a", "", "", "", "1"});
+        expectedLines.add(new String[]{"b", "", "", "", "1"});
+        expectedLines.add(new String[]{"b", "", "", "", "2"});
+        expectedLines.add(new String[]{"B", "", "", "", "2"});
+        expectedLines.add(new String[]{"c", "", "", "", "2"});
+        expectedLines.add(new String[]{"Cc", "", "", "", "2"});
+        expectedLines.add(new String[]{"cC", "", "", "", "2"});
+
+        actualLines = this.app.groupedInput(actualLines);
+
+        this.testActualAndExpected(actualLines, expectedLines);
+
+    }
+
+//    @Test
+//    public void entriesMustBeFused() {
+//        List<String[]> actualLines = new ArrayList<>();
+//        actualLines.add(new String[]{"A", "", "", "", "1"});
+//        actualLines.add(new String[]{"a", "", "", "", "1"});
+//        actualLines.add(new String[]{"a", "", "", "", "2"});
+//
+//        List<String[]> expectedLines = new ArrayList<>();
+//        expectedLines.add(new String[]{"A", "", "", "", "1"});
+//        expectedLines.add(new String[]{"a", "", "", "", "2"});
+//
+//        actualLines = this.app.fusedInput(actualLines);
+//        this.testActualAndExpected(actualLines, expectedLines);
+//    }
 }
