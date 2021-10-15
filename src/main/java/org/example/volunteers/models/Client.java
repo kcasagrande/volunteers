5ad0d851470e8b223a1dd6baf44776b31f1655c3
Léo GIGANT <rowix4@gmail.com>
2021-10-15 11:17:38 +0200

    Added client model

---
+++ src/main/java/org/example/volunteers/models/Client.java
@@ -0,0 +1,63 @@
+package org.example.volunteers.models;
+
+public class Client {
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String email;
+    private String phone;
+
+    public void Client(
+        String firstname,
+        String lastname,
+        String username,
+        String email,
+        String phone
+    ) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
