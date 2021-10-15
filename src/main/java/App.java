b813b77e4adcbbec668f0492086c1aa9ab4c5388
Léo GIGANT <rowix4@gmail.com>
2021-10-15 14:36:42 +0200

    Added process in app main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,15 @@
+import org.example.volunteers.models.ClientList;
+import org.example.volunteers.services.CsvReader;
+
+import java.io.IOException;
+import java.util.List;
+
 public class App {
-    public static void main(String[] args) {
-        // TODO
+    public static void main(String[] args) throws IOException {
+        CsvReader csvReader = new CsvReader();
+        List<String[]> fileLines = csvReader.getLinesFromFile("src/main/resources/data.csv");
+        
+        ClientList clientList = new ClientList();
+        clientList.setClientListFromFileLines(fileLines);
     }
 }
--- src/main/java/org/example/volunteers/services/CsvReader.java
+++ src/main/java/org/example/volunteers/services/CsvReader.java
@@ -8,8 +8,8 @@ import java.util.List;
 import static java.util.stream.Collectors.toList;
 
 public class CsvReader {
-    public List<String[]> getLinesFromFile() throws IOException {
-        return Files.readAllLines(Paths.get("src/main/resources/data.csv"))
+    public List<String[]> getLinesFromFile(String path) throws IOException {
+        return Files.readAllLines(Paths.get(path))
                 .stream().map(string -> string.split(";", -1))
                 .collect(toList());
     }
