f8b701943498d23e37db79289cd4d045b254e31a
Nibaldo <ndonoso@witivio.com>
2021-10-15 11:21:54 +0200

    add class contact

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,18 +1,50 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        List<String[]> lines = Files.readAllLines(Paths.get("src/main/resources/data.csv"))
+        List<String[]> personnes = Files.readAllLines(Paths.get("src/main/resources/data.csv"))
             .stream().map(string -> string.split(";"))
             .collect(toList());
 
         // Apply dark magic here...
+        // LastName
+        // Name
+        // NickName
+        // Email
+        // Phone
+        List<Contact> contacts = new ArrayList<Contact>();
 
-        System.out.println("Result goes here");
+        for (String[] _contact : personnes) {
+            int length = _contact.length;
+            if(length >= 5){
+                Contact newContact = new Contact(){{
+                    firstName = _contact[0];
+                    lastName = _contact[1];
+                    nickName = _contact[2];
+                    email = _contact[3];
+                    phone = _contact[4];
+                }};
+                contacts.add(newContact);
+            } else {
+                Contact newContact = new Contact(){{
+                    firstName = _contact[0];
+                    lastName = _contact[1];
+                    nickName = _contact[2];
+                    email = _contact[3];
+                }};
+                contacts.add(newContact);
+            }
+
+
+        }
+
+
+        System.out.println(contacts.get(0).email);
     }
 }
---
+++ src/main/java/Contact.java
@@ -0,0 +1,8 @@
+public class Contact
+{
+    public String firstName;
+    public String lastName;
+    public String nickName;
+    public String email;
+    public String phone;
+}
