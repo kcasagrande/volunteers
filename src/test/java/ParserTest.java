d66b45928a972d726594f62886c3b4d45980bf51
Valentin Mont <valentin.mont@ynov.com>
2021-10-15 14:09:11 +0200

    Implement a test to verify if the parse csv function finish with a success

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,11 +1,27 @@
+import model.PersonProperties;
 import org.junit.jupiter.api.Test;
+import service.Parser;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParserTest {
 
-   // private Parser parser;
+   private final Parser parser = new Parser();
 
     @Test
-    public void testParseCSV() {
+    public void testParseCsvFinishWithSuccess() throws IOException {
+        List<Map<PersonProperties, String>> lines =  parser.parseCsv("src/test/resources/data.csv", ";");
 
+        lines.forEach(line -> {
+                assertEquals("Rébecca", line.get(PersonProperties.firstName));
+                assertEquals("Compere", line.get(PersonProperties.lastName));
+                assertEquals("rebeccacompere@example.org", line.get(PersonProperties.email));
+                assertEquals("+33085552814", line.get(PersonProperties.phoneNumber));
+                assertEquals("", line.get(PersonProperties.userName));
+            }
+        );
     }
 }
