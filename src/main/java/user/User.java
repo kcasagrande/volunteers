cdf5023ce3b42bda834b756e59ea5960a14e625b
Thibaut <thibaut.andre277@gmail.com>
2021-10-15 11:09:04 +0200

    User object

---
+++ src/main/java/user/User.java
@@ -0,0 +1,61 @@
+package user;
+
+public class User {
+
+    // Props
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private String phone;
+
+    // Construtor
+    public User(String firstName, String lastName, String userName, String email, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.phone = phone;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
