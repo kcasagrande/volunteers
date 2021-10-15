8d1535e2ad21344fc4221952217c5c54064bf4bd
TANCHOUX Théo <tanchoux71@gmail.com>
2021-10-15 11:54:49 +0200

    sort list and format numbers

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,11 @@
+import org.example.volunteers.user.User;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -11,8 +16,48 @@ public class App {
             .stream().map(string -> string.split(";"))
             .collect(toList());
 
-        // Apply dark magic here...
+        lines.sort(Comparator.comparing(strings -> strings[0]));
 
-        System.out.println("Result goes here");
+        List<User> users = createUserListFromCSV(lines);
+
+        formatNumbers(users);
+
+        users.forEach(
+                user -> {
+                    System.out.println(user.tel);
+                }
+        );
+    }
+
+    public static List<User> createUserListFromCSV(List<String[]> csvList) {
+        List<User> users = new ArrayList<>();
+        csvList.forEach(
+                line -> {
+                    users.add(new User(line[0], line[1], line[2], line[3], line.length > 4 ? line[4] : ""));
+                }
+        );
+        return users;
+    }
+
+    public static void formatNumbers(List<User> users) {
+        users.forEach(
+                user -> {
+                    if (user.tel.contains("+33")) {
+                        user.tel = user.tel.replaceAll("\\+33", "0");
+                    }
+                    if (user.tel.contains(".")) {
+                        user.tel = user.tel.replaceAll("\\.", "");
+                    }
+                    if (user.tel.contains(" ")) {
+                        user.tel = user.tel.replaceAll(" ", "");
+                    }
+                    if (user.tel.contains("-")) {
+                        user.tel = user.tel.replaceAll("-", "");
+                    }
+                    if (user.tel.contains("(0)")) {
+                        user.tel = user.tel.replaceAll("\\(0\\)", "");
+                    }
+                }
+        );
     }
 }
---
+++ src/main/java/org/example/volunteers/user/User.java
@@ -0,0 +1,13 @@
+package org.example.volunteers.user;
+
+public class User {
+    public String surname, name, pseudo, mail, tel;
+
+    public User(String surname, String name, String pseudo, String mail, String tel) {
+        this.surname = surname;
+        this.name = name;
+        this.pseudo = pseudo;
+        this.mail = mail;
+        this.tel = tel;
+    }
+}
