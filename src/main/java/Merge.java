89775b4173b04b0b74a2468b2f4814704350ab60
GUMBAUElric <elric.gumbau@ynov.com>
2021-10-16 21:46:06 +0200

    feat(merge): create method to merge volunteers

--- src/main/java/Merge.java
+++ src/main/java/Merge.java
@@ -1,64 +1,19 @@
 import java.util.List;
 
-public class Merge{
+public class Merge {
 
-  public static List<Volunteer> MergeName(List<Volunteer> volunteers){
-    if(volunteers.get(0).name == ""){
-      for(int i =0; i<volunteers.size(); i++){
-        if(volunteers.get(i).name != "" && volunteers.get(i).name != volunteers.get(0).firstname){
-          volunteers.get(0).name = volunteers.get(i).name;
-          break;
-        }
-      }
-    }
-    return volunteers;
-  }
+    public Volunteer mergeVolunteers(List<Volunteer> comparedVolunteer) {
+      Volunteer firstVonlunteer = comparedVolunteer.get(0);
 
-  public static List<Volunteer> Mergefirstname(List<Volunteer> volunteers){
-    if(volunteers.get(0).firstname == ""){
-      for(int i =0; i<volunteers.size(); i++){
-        if(volunteers.get(i).firstname != "" && volunteers.get(i).firstname != volunteers.get(0).name){
-          volunteers.get(0).firstname = volunteers.get(i).firstname;
-          break;
-        }
+      for (Volunteer volunteer : comparedVolunteer) {
+          if (firstVonlunteer.firstname.equals("") && !volunteer.firstname.equals("")) firstVonlunteer.firstname = volunteer.firstname;
+          if (firstVonlunteer.name.equals("") && !volunteer.name.equals("")) firstVonlunteer.name = volunteer.name;
+          if (firstVonlunteer.nametag.equals("") && !volunteer.nametag.equals("")) firstVonlunteer.nametag = volunteer.nametag;
+          if (firstVonlunteer.mail.equals("") && !volunteer.mail.equals("")) firstVonlunteer.mail = volunteer.mail;
+          if (firstVonlunteer.tel.equals("") && !volunteer.tel.equals("")) firstVonlunteer.tel = volunteer.tel;
       }
-    }
-    return volunteers;
-  }
 
-  public static List<Volunteer> MergeNametag(List<Volunteer> volunteers){
-    if(volunteers.get(0).nametag == ""){
-      for(int i =0; i<volunteers.size(); i++){
-        if(volunteers.get(i).nametag != ""){
-          volunteers.get(0).nametag = volunteers.get(i).nametag;
-          break;
-        }
-      }
-    }
-    return volunteers;
-  }
+      return firstVonlunteer;
 
-  public static List<Volunteer> MergeMail(List<Volunteer> volunteers){
-    if(volunteers.get(0).mail == ""){
-      for(int i =0; i<volunteers.size(); i++){
-        if(volunteers.get(i).mail != ""){
-          volunteers.get(0).mail = volunteers.get(i).mail;
-          break;
-        }
-      }
-    }
-    return volunteers;
-  }
-
-  public static List<Volunteer> MergeTel(List<Volunteer> volunteers){
-    if(volunteers.get(0).tel == ""){
-      for(int i =0; i<volunteers.size(); i++){
-        if(volunteers.get(i).tel != ""){
-          volunteers.get(0).tel = volunteers.get(i).tel;
-          break;
-        }
-      }
-    }
-    return volunteers;
   }
 }
