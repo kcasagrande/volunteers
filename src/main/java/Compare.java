9128e1051d68e49119b18e5d99e76b754280639a
GUMBAUElric <elric.gumbau@ynov.com>
2021-10-16 20:42:50 +0200

    refactor(compare): create method compareNameInMail

--- src/main/java/Compare.java
+++ src/main/java/Compare.java
@@ -3,72 +3,18 @@ import java.util.List;
 
 public class Compare {
 
-  public List<Volunteer> volunteers = new ArrayList<Volunteer>();
-  public List<Volunteer> comparedVolunteers = new ArrayList<Volunteer>();
+    public static List<Volunteer> arrayVolunteers = new ArrayList<Volunteer>();
 
-  public Compare(List<Volunteer> newVolunteers) {
-      this.volunteers = newVolunteers;
-  }
+    public List<Volunteer> compareNameInMail(List<Volunteer> volunteers, String volunterrFirstName) {
 
-  public List<Volunteer> CompareNum(String volunteerNum){
+        List<Volunteer> arrayToReturn = new ArrayList<Volunteer>();
 
-    for(int index = 0; index < volunteers.size() ; index ++ ){
-      if (volunteerNum == volunteers.get(index).tel){
-        this.comparedVolunteers.add(volunteers.get(index));
-      }
+
+        for (Volunteer volunteer : volunteers) {
+            if (volunteer.mail.contains(volunterrFirstName.toLowerCase())) arrayToReturn.add(volunteer);
+        }
+
+        return arrayToReturn;
+
     }
-    return this.comparedVolunteers;
-  }
-
-  public List<Volunteer> CompareMail(List<Volunteer> comparedVolunteers, String volunteerMail){
-
-    for(int index = 0; index < volunteers.size() ; index++ ){
-      if (volunteerMail == volunteers.get(index).mail){
-        this.comparedVolunteers.add(volunteers.get(index));
-      }
-    }
-    return comparedVolunteers;
-  }
-
-  public List<Volunteer> CompareFirstName(List<Volunteer> comparedVolunteers,String volunteerFirstName){
-
-    for(int index = 0; index < comparedVolunteers.size() ; index++ ){
-      if (volunteerFirstName != volunteers.get(index).firstname){
-        this.CompareNameByFirstName(comparedVolunteers, volunteerFirstName);
-      }
-    }
-    return comparedVolunteers;
-  }
-
-  public List<Volunteer> CompareName(List<Volunteer> comparedVolunteers, String volunteerName){
-
-    for(int index = 0; index < comparedVolunteers.size() ; index++ ){
-      if (volunteerName != volunteers.get(index).name){
-        this.CompareFirstNameByName(comparedVolunteers, volunteerName);
-      }
-    }
-    return comparedVolunteers;
-  }
-
-  public List<Volunteer> CompareFirstNameByName(List<Volunteer> comparedVolunteers, String volunteerName){
-
-    for(int index = 0; index < comparedVolunteers.size() ; index++ ){
-      if (volunteerName != volunteers.get(index).firstname){
-        comparedVolunteers.remove(index);
-        index = index--;
-      }
-    }
-    return this.comparedVolunteers;
-  }
-
-  public List<Volunteer> CompareNameByFirstName(List<Volunteer> comparedVolunteers, String volunteerName){
-
-    for(int index = 0; index < comparedVolunteers.size() ; index++ ){
-      if (volunteerName != volunteers.get(index).firstname){
-        comparedVolunteers.remove(index);
-        index = index--;
-      }
-    }
-    return comparedVolunteers;
-  }
 }
