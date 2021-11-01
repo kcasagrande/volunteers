cbdd26083c0efb6fff37ad305a616b26558006fd
dherve <dherve@iidre.com>
2021-11-01 17:22:58 +0100

    Ajout test avec empty fields.

--- src/test/java/MergeTest.java
+++ src/test/java/MergeTest.java
@@ -33,7 +33,7 @@ public class MergeTest {
     }
 
     @Test
-    public void mergeSameUserWithSameRecurrentFirstname(){
+    public void mergeSameUserWithSameRecurrentFields(){
         User userA = new User("About", "Axelle","Axelle","about@example.net","0000555503");
         User userB = new User("About","Axele","","axelleabout@example.net","0000555503");
         User userC = new User("About","Axell","","axelle_about@example.org","0000555503");
@@ -50,6 +50,27 @@ public class MergeTest {
 
         assertEquals(1, merge.getResult().size());
         assertEquals(new User("About", "Axelle", "Axelle", "about@example.net", "0000555503"),
+                merge.getResult().get(0));
+    }
+
+    @Test
+    public void mergeSameUserWithEmptyFields(){
+        User userA = new User("About", "Axelle","Axelle","","0000555503");
+        User userB = new User("About","","","","0000555503");
+        User userC = new User("","","","axelle_about@example.org","");
+        User userD = new User("About","","","","");
+
+
+        Merge merge = new Merge("About");
+        merge.addUser(userA);
+        merge.addUser(userB);
+        merge.addUser(userD);
+        merge.addUser(userC);
+
+        merge.mergeUsers();
+
+        assertEquals(1, merge.getResult().size());
+        assertEquals(new User("About", "Axelle", "Axelle", "axelle_about@example.org", "0000555503"),
                 merge.getResult().get(0));
     }
 }
