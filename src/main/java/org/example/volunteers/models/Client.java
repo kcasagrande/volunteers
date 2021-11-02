ba8477e3219940d0e91336dac341b17fdc7f2d8a
Léo GIGANT <rowix4@gmail.com>
2021-11-02 11:11:42 +0100

    Removed unused setters for Client model

--- src/main/java/org/example/volunteers/models/Client.java
+++ src/main/java/org/example/volunteers/models/Client.java
@@ -25,23 +25,13 @@ public class Client {
 
     public String getFirstname() {return firstname;}
 
-    public void setFirstname(String firstname) {this.firstname = firstname;}
-
     public String getLastname() {return lastname;}
-
-    public void setLastname(String lastname) {this.lastname = lastname;}
 
     public String getUsername() {return username;}
 
-    public void setUsername(String username) {this.username = username;}
-
     public String getEmail() {return email;}
 
-    public void setEmail(String email) {this.email = email;}
-
     public String getPhone() {return phone;}
-
-    public void setPhone(String phone) {this.phone = phone;}
 
     private void normalizePhoneNumber() {
         String number = this.phone;
