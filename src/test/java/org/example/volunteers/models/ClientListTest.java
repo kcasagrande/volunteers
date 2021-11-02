9f6918e6ccc3b6088d68314e46592d46cb467eb4
Léo GIGANT <rowix4@gmail.com>
2021-11-02 14:27:18 +0100

    Added test for getting client list from list of strings

--- src/test/java/org/example/volunteers/models/ClientListTest.java
+++ src/test/java/org/example/volunteers/models/ClientListTest.java
@@ -1,4 +1,35 @@
 package org.example.volunteers.models;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ClientListTest {
+    @Test
+    public void testGetClientListFromListOfStrings() {
+        List<String[]> listClients = new ArrayList<>();
+        listClients.add(new String[]{
+                "Arthur",
+                "Genthial",
+                "Volt",
+                "a@a.com",
+                "+336 17 33 25 45"
+        });
+
+        ClientList clientList = new ClientList();
+        clientList.setClientListFromFileLines(listClients);
+        List<Client> resultList = clientList.getClientList();
+
+        Client client = resultList.get(0);
+
+        assertEquals(1, resultList.size());
+        assertEquals("Arthur", client.getFirstname());
+        assertEquals("Genthial", client.getLastname());
+        assertEquals("Volt", client.getUsername());
+        assertEquals("a@a.com", client.getEmail());
+        assertEquals("0617332545", client.getPhone());
+    }
 }
