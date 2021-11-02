8691acea7e0043bd8a21ae5d6d7fb8494109afe2
Léo GIGANT <rowix4@gmail.com>
2021-11-02 16:15:29 +0100

    Added tests cases for csv reader test method

--- src/test/java/org/example/volunteers/services/CsvReaderTest.java
+++ src/test/java/org/example/volunteers/services/CsvReaderTest.java
@@ -2,9 +2,10 @@ package org.example.volunteers.services;
 
 import java.io.IOException;
 import java.util.List;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CsvReaderTest {
     @Test
@@ -14,5 +15,7 @@ public class CsvReaderTest {
         List<String[]> data = reader.getLinesFromFile("src/test/java/org/example/volunteers/services/data-test.csv");
 
         assertEquals(4, data.size());
+        assertEquals("+33(0)0 55 53 75 36", data.get(0)[4]);
+        assertEquals("", data.get(3)[2]);
     }
 }
