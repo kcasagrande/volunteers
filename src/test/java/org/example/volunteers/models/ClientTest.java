c541717d7ae0aba9aca618a57f453676ec67d21b
Léo GIGANT <rowix4@gmail.com>
2021-11-02 14:14:33 +0100

    Optimized tests for Client model

--- src/test/java/org/example/volunteers/models/ClientTest.java
+++ src/test/java/org/example/volunteers/models/ClientTest.java
@@ -5,35 +5,53 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ClientTest {
-    @Test
-    public void testNormalizePhoneNumber() {
-        String expectedPhoneNumber = "0768580037";
+    private static final String expectedPhoneNumber = "0768580037";
 
+    @Test
+    public void testNormalizePhoneNumberWithSpaces() {
+        Client client = new Client("", "","","","07 68 58 00 37");
+        assertEquals(client.getPhone(), expectedPhoneNumber);
+    }
+
+    @Test
+    public void testNormalizePhoneNumberWithPoints() {
+        Client client = new Client("", "","","","07.68.58.00.37");
+        assertEquals(client.getPhone(), expectedPhoneNumber);
+    }
+
+    @Test
+    public void testNormalizePhoneNumberWithIndicatorAndPoints() {
+        Client client = new Client("", "","","","+337.68.58.00.37");
+        assertEquals(client.getPhone(), expectedPhoneNumber);
+    }
+
+    @Test
+    public void testNormalizePhoneNumberWithIndicatorAndFirstZeroBracketsDashLastSpacePoint() {
         Client client = new Client("", "","","","+33 (0) 7 68-58 00.37");
         assertEquals(client.getPhone(), expectedPhoneNumber);
+    }
 
-        client = new Client("", "","","","+33 0 7 68-58 00.37");
+    @Test
+    public void testNormalizePhoneNumberWithIndicator() {
+        Client client = new Client("", "","","","+33768580037");
         assertEquals(client.getPhone(), expectedPhoneNumber);
+    }
 
-        client = new Client("", "","","","+33 7 68-58 00.37");
+    @Test
+    public void testNormalizePhoneNumberWithIndicatorAndSpaces() {
+        Client client = new Client("", "","","","+33 7 68 58 00 37");
         assertEquals(client.getPhone(), expectedPhoneNumber);
+    }
 
-        client = new Client("", "","","","+33 768580037");
+    @Test
+    public void testNormalizePhoneNumberWithDashes() {
+        Client client = new Client("", "","","","07-68-58-00-37");
         assertEquals(client.getPhone(), expectedPhoneNumber);
+    }
 
-        client = new Client("", "","","","+33768580037");
-        assertEquals(client.getPhone(), expectedPhoneNumber);
-
-        client = new Client("", "","","","07 68 58 00 37");
-        assertEquals(client.getPhone(), expectedPhoneNumber);
-
-        client = new Client("", "","","","07.68.58.00.37");
-        assertEquals(client.getPhone(), expectedPhoneNumber);
-
-        client = new Client("", "","","","07-68-58-00-37");
-        assertEquals(client.getPhone(), expectedPhoneNumber);
-
-        client = new Client("", "","","","+33 7 68 58 00 37");
+    @Test
+    public void testNormalizePhoneNumberSpacesDashPoint() {
+        Client client = new Client("", "","","","+33 0 7 68-58 00.37");
         assertEquals(client.getPhone(), expectedPhoneNumber);
     }
 }
