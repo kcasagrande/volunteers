c9c33b9432c4cbb3e1e8d375cd95cac0816f3d7a
Léo GIGANT <rowix4@gmail.com>
2021-11-02 11:38:45 +0100

    Added tests for formalize phone number method

--- src/test/java/org/example/volunteers/models/ClientTest.java
+++ src/test/java/org/example/volunteers/models/ClientTest.java
@@ -1,4 +1,33 @@
 package org.example.volunteers.models;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ClientTest {
+    @Test
+    public void testNormalizePhoneNumber() {
+        String expectedPhoneNumber = "0768580037";
+
+        Client client = new Client("", "","","","+33 (0) 7 68-58 00.37");
+        assertEquals(client.getPhone(), expectedPhoneNumber);
+
+        client = new Client("", "","","","+33 0 7 68-58 00.37");
+        assertEquals(client.getPhone(), expectedPhoneNumber);
+
+        client = new Client("", "","","","+33 7 68-58 00.37");
+        assertEquals(client.getPhone(), expectedPhoneNumber);
+
+        client = new Client("", "","","","+33 768580037");
+        assertEquals(client.getPhone(), expectedPhoneNumber);
+
+        client = new Client("", "","","","+33768580037");
+        assertEquals(client.getPhone(), expectedPhoneNumber);
+
+        client = new Client("", "","","","07 68 58 00 37");
+        assertEquals(client.getPhone(), expectedPhoneNumber);
+
+        client = new Client("", "","","","+33 7 68 58 00 37");
+        assertEquals(client.getPhone(), expectedPhoneNumber);
+    }
 }
