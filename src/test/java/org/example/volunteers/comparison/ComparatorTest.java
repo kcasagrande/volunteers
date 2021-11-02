cba00f2940ca14ff0beaabac3a6047a0b725b29a
Aexandre <alexandre.gelin@ynov.com>
2021-11-02 11:56:51 +0100

    add test with pseudo

--- src/test/java/org/example/volunteers/comparison/ComparatorTest.java
+++ src/test/java/org/example/volunteers/comparison/ComparatorTest.java
@@ -51,14 +51,14 @@ public class ComparatorTest {
         List<String[]> lines = new ArrayList<String[]>();
         String[] a1 = new String[]{"Jean", "Bon"," ", "jeanbon@example.org",""};
         lines.add(0, a1);
-        String[] a2 = new String[]{"Bon", "Jean"," ", "jeanbon@example.org",""};
+        String[] a2 = new String[]{"Bon", "Jean","Twizou ", "jeanbon@example.org",""};
         lines.add(1, a2);
         Comparator comparator = new Comparator();
         result = comparator.CompareList(lines);
         assertEquals("same", result);
         assertEquals(1, lines.size());
     }
-
+    // comparaison numpero telephone + bien suppr de la liste
     @Test
     public void compareListPhone(){
         String result;
@@ -66,6 +66,36 @@ public class ComparatorTest {
         String[] a1 = new String[]{"Jean", "Bon","", "jeanbon@example.org", "+33055541000"};
         lines.add(0, a1);
         String[] a2 = new String[]{"Bon", "Jean","", "jeanbon@example.org", "+33055541000"};
+        lines.add(1, a2);
+        Comparator comparator = new Comparator();
+        result = comparator.CompareList(lines);
+        assertEquals("same", result);
+        assertEquals(1, lines.size());
+    }
+
+    //ne supprime pas quand le pseudo est vide
+    @Test
+    public void comparePseudoEmpty(){
+        String result;
+        List<String[]> lines = new ArrayList<String[]>();
+        String[] a1 = new String[]{"Jean", "Bon","", "jeanbon@example.org", "+33055541000"};
+        lines.add(0, a1);
+        String[] a2 = new String[]{"", "Jean","", "jeanbon@example.net", "+33055542000"};
+        lines.add(1, a2);
+        Comparator comparator = new Comparator();
+        result = comparator.CompareList(lines);
+        assertEquals("not same", result);
+        assertEquals(2, lines.size());
+    }
+
+    // compare le pseudo avec les autres champs
+    @Test
+    public void comparePseudoChamps(){
+        String result;
+        List<String[]> lines = new ArrayList<String[]>();
+        String[] a1 = new String[]{"Jean", "Bon","Twizou", "jeanbon@example.org", "+33055541000"};
+        lines.add(0, a1);
+        String[] a2 = new String[]{"Twizou", "","", "jeanbon@example.net", "+33055542000"};
         lines.add(1, a2);
         Comparator comparator = new Comparator();
         result = comparator.CompareList(lines);
