09ce637164f66a0d2ba31f651f5e1968cd82bac7
Nibaldo <ndonoso@witivio.com>
2021-11-02 14:53:12 +0100

    loop test for contact list

--- src/test/java/ContactTest.java
+++ src/test/java/ContactTest.java
@@ -20,92 +20,112 @@ public class ContactTest {
     @BeforeEach
     public void setUp() throws IOException {
         dependency = (String string) -> 1;
-        List<String[]> personnes = Files.readAllLines(Paths.get("src/main/resources/data.csv"))
+        List<String[]> persons = Files.readAllLines(Paths.get("src/main/resources/data.csv"))
                 .stream().map(string -> string.split(";"))
                 .collect(toList());
 
-        ArrayList<Contact> contacts = App.CreateContactList(personnes);
+        ArrayList<Contact> contacts = App.CreateContactList(persons);
         contacts_test = new ArrayList<Contact>(App.removeDuplicates(contacts));
     }
 
     @Test
     public void checkFirstPerson() {
-        // Arrange
-        Contact person = contacts_test.get(0);
-        System.out.println(person.firstName.concat(" " + person.lastName).concat(" " + person.email));
+        for (Contact contact: contacts_test) {
+            Contact person = getContactWithEmailNotEmpty(contact);
+            if(person != null){
+                System.out.println(person.firstName.concat(" " + person.lastName).concat(" " + person.email));
 
-        // Assert
-        assertEquals("rao", person.firstName.toLowerCase(Locale.ROOT), "Ce message s'affiche si le test échoue");
-    }
-
-    public Contact getContactWithEmailNotEmpty(){
-        int i = 0;
-        Contact person = contacts_test.get(0);
-        boolean emailEmpty = person.email.isEmpty();
-        while(!emailEmpty){
-            if(!contacts_test.get(i).email.isEmpty()){
-                person = contacts_test.get(i);
-            }else{
-                i++;
+                // Assert
+                assertEquals(person.firstName.toLowerCase(Locale.ROOT).toString(), person.firstName.toLowerCase(Locale.ROOT), "The firstName is ok.");
             }
         }
-        return person;
     }
 
     @Test
     public void checkIfEmailIsCorrect() {
-        // Arrange
-        Contact person = getContactWithEmailNotEmpty();
-
-        // Assert
-        assertEquals(true, Contact.isValidEmail(person.email), "Ce message s'affiche si le test échoue");
+        for (Contact contact: contacts_test) {
+            Contact person = getContactWithEmailNotEmpty(contact);
+            if(person != null) {
+                System.out.println(String.format("The email is valid: %s", person.getEmail().toString()));
+                // Assert
+                assertEquals(true, Contact.isValidEmail(person.getEmail()), String.format("Email is: %s", person.getEmail().toString()));
+            }
+        }
     }
 
     @Test
     public void checkIsNameInEmail(){
-        // Arrange
-        String email = getContactWithEmailNotEmpty().email;
-        System.out.println(email);
+        for (Contact contact: contacts_test) {
+            Contact person = getContactWithEmailNotEmpty(contact);
+            if(person != null) {
+                String email = person.getEmail();
+                if(!email.isEmpty()){
+                    System.out.println(String.format("The email is: %s", email));
 
-        String firstName = getContactWithEmailNotEmpty().firstName;
-        System.out.println(firstName);
+                    String firstName = person.firstName;
+                    System.out.println(String.format("The firstName is: %s", firstName));
 
-        assertEquals(true, email.contains(Contact.removeDiacriticalMarks(firstName.toLowerCase(Locale.ROOT))), "Le firstName n'est pas dans le email");
+                    assertEquals(true, email.contains(Contact.removeDiacriticalMarks(firstName.toLowerCase(Locale.ROOT))), "Le firstName n'est pas dans le email");
+                }
+            }
+        }
     }
 
     @Test
     public void checkIsLastNameInEmail(){
-        // Arrange
-        String email = getContactWithEmailNotEmpty().email;
-        System.out.println(email);
+        for (Contact contact: contacts_test) {
+            Contact person = getContactWithEmailNotEmpty(contact);
+            if(person != null) {
+                String email = person.getEmail();
+                if(!email.isEmpty()){
+                    System.out.println(String.format("The email is: %s", email));
 
-        String lastName = getContactWithEmailNotEmpty().lastName;
-        System.out.println(lastName);
+                    String lastName = person.lastName;
+                    System.out.println(String.format("The lastName is: %s", lastName));
 
-        assertEquals(true, email.contains(Contact.removeDiacriticalMarks(lastName.toLowerCase(Locale.ROOT))), "Le lastName n'est pas dans le email");
+                    assertEquals(true, email.contains(Contact.removeDiacriticalMarks(lastName.toLowerCase(Locale.ROOT))), "Le lastName n'est pas dans le email");
+                }
+            }
+        }
     }
 
     @Test
     public void checkIsNickNameInEmail(){
-        // Arrange
-        String email = getContactWithEmailNotEmpty().email;
-        System.out.println(email);
+        for (Contact contact: contacts_test) {
+            Contact person = getContactWithEmailNotEmpty(contact);
+            if(person != null) {
+                String email = person.getEmail();
+                if(!email.isEmpty()){
+                    System.out.println(String.format("The email is: %s", email));
 
-        String nickName = getContactWithEmailNotEmpty().nickName;
-        System.out.println("nickname: " + nickName);
+                    String nickName = person.nickName;
+                    System.out.println(String.format("The nickName is: %s", nickName));
 
-
-        assertEquals(true, email.contains(Contact.removeDiacriticalMarks(nickName.toLowerCase(Locale.ROOT))), "Le lastName n'est pas dans le email");
+                    assertEquals(true, email.contains(Contact.removeDiacriticalMarks(nickName.toLowerCase(Locale.ROOT))), "Le nickName n'est pas dans le email");
+                }
+            }
+        }
     }
 
     @Test
     public void checkIfPhoneIsCorrect() {
-        // Arrange
-        String phoneToCheck = contacts_test.get(0).phone;
+        for (Contact contact: contacts_test) {
+            Contact person = getContactWithEmailNotEmpty(contact);
+            if(person != null) {
+                String phoneToCheck = person.phone;
+                System.out.println(String.format("The phone is: %s", phoneToCheck));
 
-        // Assert
-        System.out.println(phoneToCheck);
-        assertEquals(true, Contact.checkIsValidNumberPhone(phoneToCheck), "Ce message s'affiche si le test échoue");
+                // Assert
+                assertEquals(true, Contact.checkIsValidNumberPhone(phoneToCheck), String.format("The phone is %s", phoneToCheck.toString()));
+            }
+        }
     }
 
+    private Contact getContactWithEmailNotEmpty(Contact contact){
+        Contact person = contact;
+        if(!person.getEmail().isEmpty()){
+            return person;
+        }
+        return null;
+    }
 }
