7bd6f2fa1c7fde3ac549464abf5d94db968fc729
Nicolas Barbosa <nicolas.barbosa@orange.com>
2022-07-11 15:24:11 +0200

    add phone correction

--- src/test/java/org/example/volunteers/CleanerTest.java
+++ src/test/java/org/example/volunteers/CleanerTest.java
@@ -11,13 +11,48 @@ public class CleanerTest {
 
     @Test
     public void useNameAsNickname() {
-        givenVolunteer(new Volunteer("Hugo", "Bordais", "", "hugo.bordais@cuck.com", "0700000000"));
+        givenVolunteer("Hugo", "Bordais", "", "hugo.bordais@cuck.com", "+33070000000");
         whenCleaningUpVolunteers();
         thenNickNameIs("Hugo");
     }
 
-    private void givenVolunteer(Volunteer volunteer) {
-        volunteers.add(volunteer);
+    @Test
+    public void transformPhoneNumberInFrenchFormat() {
+        givenVolunteer("test", "test", "test", "test@test.test", "00-55-51-64-64");
+        whenCleaningUpVolunteers();
+        thenPhoneIs("+33055516464");
+    }
+
+    @Test
+    public void transformPhoneNumberWith33AtBeginningInFrenchFormat() {
+        givenVolunteer("test", "test", "test", "test@test.test", "+33(0)0-55-55-78-08");
+        whenCleaningUpVolunteers();
+        thenPhoneIs("+33055557808");
+    }
+
+    @Test
+    public void transformPhoneNumberWith33InsideInFrenchFormat() {
+        givenVolunteer("test", "test", "test", "test@test.test", "+33(0)0-55-33-78-08");
+        whenCleaningUpVolunteers();
+        thenPhoneIs("+33055337808");
+    }
+
+    @Test
+    public void transformPhoneNumberWith33InsideInFrenchFormat2() {
+        givenVolunteer("test", "test", "test", "test@test.test", "(0)0-55-33-78-08");
+        whenCleaningUpVolunteers();
+        thenPhoneIs("+33055337808");
+    }
+
+    @Test
+    public void transformPhoneNumberWithDotsInsideFrenchFormat() {
+        givenVolunteer("test", "test", "test", "test@test.test", "+330.45.55.69.06");
+        whenCleaningUpVolunteers();
+        thenPhoneIs("+33045556906");
+    }
+
+    private void givenVolunteer(String firstName, String lastName, String nickname, String eMail, String phone) {
+        volunteers.add(new Volunteer(firstName, lastName, nickname, eMail, phone));
     }
 
     private void whenCleaningUpVolunteers() {
