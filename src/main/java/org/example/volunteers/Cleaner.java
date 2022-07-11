2659fb32101652bcb1eeca31b3cf24370cc58457
Maxence Deschamps <maxence.deschamps.pro@gmail.com>
2022-07-11 15:04:50 +0200

    ajout d'une méthode pour supprimer les doublons

--- src/main/java/org/example/volunteers/Cleaner.java
+++ src/main/java/org/example/volunteers/Cleaner.java
@@ -1,43 +1,59 @@
 package org.example.volunteers;
 
-import org.testng.annotations.Test;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
-
-import static org.testng.Assert.assertEquals;
+import java.util.Objects;
 
 public class Cleaner {
     public static List<Volunteer> cleanUp(List<Volunteer> volunteers) {
-        // This function should contain your dark magic.
-        // For now, it simply returns a copy of the initial list.
+        removeDuplicate(volunteers);
+
         return new ArrayList<>(volunteers);
     }
 
-
-
-    public static List<Volunteer> removeDuplicateFirstNameLastNamePseudoMailPhone(List<Volunteer> volunteers) {
-
-
-        LinkedHashSet<String> linkedsetVolunteers = new LinkedHashSet<String>();
+    public static List<Volunteer> removeDuplicate(List<Volunteer> volunteers) {
+        List<Volunteer> cleanedVolunteers = new ArrayList<>();
 
         for (Volunteer volunteer: volunteers) {
-            linkedsetVolunteers.add(volunteer.toString());
+            if (cleanedVolunteers.size() == 0) {
+                cleanedVolunteers.add(volunteer);
+                continue;
+            }
+
+            Boolean addVolunteer = true;
+            for (Volunteer cleanedVolunteer: cleanedVolunteers) {
+                if (Objects.equals(cleanedVolunteer.toString(), volunteer.toString())) {
+                    addVolunteer = false;
+                }
+            }
+
+            if (addVolunteer) {
+                cleanedVolunteers.add(volunteer);
+            }
         }
 
-        volunteers.clear();
-
-        for (String str_volunteer: linkedsetVolunteers) {
-            String[] volunteerSplit = str_volunteer.split(";");
-            Volunteer newVolunteer = new Volunteer(volunteerSplit[0], volunteerSplit[1], volunteerSplit[2], volunteerSplit[3], volunteerSplit[4]);
-            volunteers.add(newVolunteer);
-        }
-        return volunteers;
-
+        return cleanedVolunteers;
     }
 
 
-
+//    public static List<Volunteer> removeDuplicateFirstNameLastNamePseudoMailPhone(List<Volunteer> volunteers) {
+//
+//
+//        LinkedHashSet<String> linkedsetVolunteers = new LinkedHashSet<String>();
+//
+//        for (Volunteer volunteer: volunteers) {
+//            linkedsetVolunteers.add(volunteer.toString());
+//        }
+//
+//        volunteers.clear();
+//
+//        for (String str_volunteer: linkedsetVolunteers) {
+//            String[] volunteerSplit = str_volunteer.split(";");
+//            Volunteer newVolunteer = new Volunteer(volunteerSplit[0], volunteerSplit[1], volunteerSplit[2], volunteerSplit[3], volunteerSplit[4]);
+//            volunteers.add(newVolunteer);
+//        }
+//        return volunteers;
+//
+//    }
 }
--- src/test/java/org/example/volunteers/CleanerTest.java
+++ src/test/java/org/example/volunteers/CleanerTest.java
@@ -7,25 +7,33 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class CleanerTest {
 
-
-
-
     @Test
-    public void testRemovedDuplicateVerifyFirstNameLastNameNicknamePseudoMailPhone() {
-
+    public void shouldRemoveDuplicateWhenAllDataAreTheSame() {
         List<Volunteer> volunteers = new ArrayList<>();
-        volunteers.add(new Volunteer("doe", "john", "jojo2", "john@mail.com", "+33698675434"));
-        volunteers.add(new Volunteer("doe", "john", "jojo", "john@mail.com", "+33698675434"));
-        volunteers.add(new Volunteer("doe", "john", "jojo", "john@mail.com", "+33698675434"));
+        volunteers.add(new Volunteer("Nom", "Prenom", "pseudo", "email@gmail.com", "+33600000000"));
+        volunteers.add(new Volunteer("Nom", "Prenom", "pseudo", "email@gmail.com", "+33600000000"));
+        volunteers.add(new Volunteer("Nom2", "Prenom2", "pseudo2", "email2@gmail.com", "+33600000002"));
+        volunteers.add(new Volunteer("Nom3", "Prenom3", "pseudo3", "email3@gmail.com", "+33600000003"));
+        volunteers.add(new Volunteer("Nom2", "Prenom2", "pseudo2", "email2@gmail.com", "+33600000002"));
 
-        List<Volunteer> result = Cleaner.removeDuplicateFirstNameLastNamePseudoMailPhone(volunteers);
+        List<Volunteer> cleanedVolunteers = Cleaner.removeDuplicate(volunteers);
 
-        assertEquals(2, result.size());
+        assertEquals(3, cleanedVolunteers.size(), "La liste nettoyée doit être de taille 3");
     }
 
-
+//    @Test
+//    public void testRemovedDuplicateVerifyFirstNameLastNameNicknamePseudoMailPhone() {
+//
+//        List<Volunteer> volunteers = new ArrayList<>();
+//        volunteers.add(new Volunteer("doe", "john", "jojo2", "john@mail.com", "+33698675434"));
+//        volunteers.add(new Volunteer("doe", "john", "jojo", "john@mail.com", "+33698675434"));
+//        volunteers.add(new Volunteer("doe", "john", "jojo", "john@mail.com", "+33698675434"));
+//
+//        List<Volunteer> result = Cleaner.removeDuplicateFirstNameLastNamePseudoMailPhone(volunteers);
+//
+//        assertEquals(2, result.size());
+//    }
 
 }
