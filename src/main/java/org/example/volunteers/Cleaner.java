b337f02b13b050abe5021c3c80ad9294c69328e3
Robin OGER <oger.robin@gmail.com>
2022-07-11 15:01:27 +0200

    feat: email validator

--- src/main/java/org/example/volunteers/Cleaner.java
+++ src/main/java/org/example/volunteers/Cleaner.java
@@ -1,12 +1,18 @@
 package org.example.volunteers;
 
+import com.sun.tools.javac.util.StringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Cleaner {
     public static List<Volunteer> cleanUp(List<Volunteer> volunteers) {
         // This function should contain your dark magic.
         // For now, it simply returns a copy of the initial list.
         return new ArrayList<>(volunteers);
+    }
+    public static Boolean isValidEmail(Volunteer volunteer) {
+        return Pattern.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", volunteer.eMail);
     }
 }
--- src/test/java/org/example/volunteers/CleanerTest.java
+++ src/test/java/org/example/volunteers/CleanerTest.java
@@ -1,6 +1,20 @@
 package org.example.volunteers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CleanerTest {
+    @Test
+    public void validEmailAddressShouldVeValidated() {
+        Volunteer volunteer = new Volunteer("firstName", "lastName", "nickname", "adresse.mail@mail.com", "123456");
+        boolean isEmail = Cleaner.isValidEmail(volunteer);
+        assertTrue(isEmail, "Une adresse email valide est validée");
+    }
+    @Test
+    public void invalidEmailShouldNotBeValidated() {
+        Volunteer volunteer = new Volunteer("firstName", "lastName", "nickname", "adresse.mailmailcom", "123456");
+        boolean isEmail = Cleaner.isValidEmail(volunteer);
+        assertFalse(isEmail, "Une adresse email invalide n'est pas validée");
+    }
 }
