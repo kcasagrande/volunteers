d24a8b79989bc211658bfc02e60d6b559864561f
Jean-Philippe Bourdais <jeanphi.bourdais@gmail.com>
2022-07-12 17:04:10 +0200

    add new tests for concat data

--- src/test/java/org/example/volunteers/DuplicateTest.java
+++ src/test/java/org/example/volunteers/DuplicateTest.java
@@ -43,4 +43,30 @@ public class DuplicateTest {
 
         assertEquals(2, result.size(), "La liste ne doit pas garder le doublon sur le téléphone +33698675434 car le numéro de téléphone est similaire");
     }
+
+    @Test
+    public void concatDuplicateMail() {
+        List<Volunteer> volunteers = new ArrayList<>();
+        volunteers.add(new Volunteer("john", "doe", "jojo", "john@mail.com", "+33698675434"));
+        volunteers.add(new Volunteer("john", "doe", "jojo", "john1@mail.com", "+33698675434"));
+        volunteers.add(new Volunteer("john", "doe", "jojo", "john2@mail.com", "+33698675434"));
+
+        List<Volunteer> volunteersResult = Duplicate.concatDuplicateMailPhone(volunteers);
+
+        assertEquals(1, volunteersResult.size(), "La taille du tableau devrait etre 1");
+        assertEquals("john@mail.com,john1@mail.com,john2@mail.com", volunteersResult.get(0).eMail, "Les emails devraient etre concatenes en 'john@mail.com,john1@mail.com,john2@mail.com'");
+    }
+
+    @Test
+    public void concatDuplicatePhone() {
+        List<Volunteer> volunteers = new ArrayList<>();
+        volunteers.add(new Volunteer("john", "doe", "jojo", "john@mail.com", "+33698675431"));
+        volunteers.add(new Volunteer("john", "doe", "jojo", "john@mail.com", "+33698675432"));
+        volunteers.add(new Volunteer("john", "doe", "jojo", "john@mail.com", "+33698675433"));
+
+        List<Volunteer> volunteersResult = Duplicate.concatDuplicateMailPhone(volunteers);
+
+        assertEquals(1, volunteersResult.size(), "La taille du tableau devrait etre 1");
+        assertEquals("+33698675431,+33698675432,+33698675433", volunteersResult.get(0).phone, "Les numéros de téléphone devraient etre concatenes en '+33698675431,+33698675432,+33698675433'");
+    }
 }
