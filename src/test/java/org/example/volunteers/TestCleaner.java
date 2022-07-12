6c3854abca32b447d65734056964f499dc10562f
Guillaume Chx <guillaume.chateauroux@gmail.com>
2022-07-12 16:15:01 +0200

    refactor: increased cleanup test readability

--- src/test/java/org/example/volunteers/TestCleaner.java
+++ src/test/java/org/example/volunteers/TestCleaner.java
@@ -1,10 +1,10 @@
 package org.example.volunteers;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TestCleaner {
@@ -12,7 +12,6 @@ public class TestCleaner {
     // Integration test
     @Test
     public void shouldCallEachFieldSanitizerForEachVolunteers() {
-        List<Volunteer> volunteers = new ArrayList<>();
         volunteers.add(new Volunteer("leo", "duff", "duff", "leo.duff@voleur.ytb", "+330069696969"));
         FieldsSanitizerTesting fieldsSanitizer = new FieldsSanitizerTesting();
 
@@ -26,106 +25,110 @@ public class TestCleaner {
     // Unit tests
     @Test
     public void shouldRegroupMails() {
-        List<Volunteer> volunteers = new ArrayList<>();
-        volunteers.add(new Volunteer("leo", "duff", "duff", "leo.duff@voleur.ytb", "+330069696969"));
-        volunteers.add(new Volunteer("LEO", "duff", "duff", "leo.duff@voleur.twitch", "+330069696969"));
-        volunteers.add(new Volunteer("leo", "DUFF", "duff", "leo.duff@voleur.dailmotion", "+330069696969"));
+        addVolunteers(
+                new Volunteer("leo", "duff", "duff", "leo.duff@voleur.ytb", "+330069696969"),
+                new Volunteer("LEO", "duff", "duff", "leo.duff@voleur.twitch", "+330069696969"),
+                new Volunteer("leo", "DUFF", "duff", "leo.duff@voleur.dailmotion", "+330069696969")
+        );
 
         List<Volunteer> expectedVolunteers = new ArrayList<>();
         expectedVolunteers.add(new Volunteer("leo", "duff", "duff", "leo.duff@voleur.ytb;leo.duff@voleur.twitch;leo.duff@voleur.dailmotion", "0069696969"));
 
-        FieldsSanitizerTesting fieldsSanitizer = new FieldsSanitizerTesting();
-
-        List<Volunteer> resultVolunteers = Cleaner.cleanUp(volunteers, fieldsSanitizer);
-
-        Assertions.assertEquals(expectedVolunteers.toString(), resultVolunteers.toString());
+        whenCleaningVolunteers();
+        thenExpectedListIs(expectedVolunteers);
     }
 
     @Test
     public void shouldRegroupNickNames() {
-        List<Volunteer> volunteers = new ArrayList<>();
-        volunteers.add(new Volunteer("leo", "duff", "duff", "leo.duff@voleur.ytb", "+330069696969"));
-        volunteers.add(new Volunteer("LEO", "duff", "miguel", "leo.duff@voleur.ytb", "+330069696969"));
-        volunteers.add(new Volunteer("leo", "DUFF", "zera", "leo.duff@voleur.ytb", "+330069696969"));
+        addVolunteers(
+                new Volunteer("leo", "duff", "duff", "leo.duff@voleur.ytb", "+330069696969"),
+                new Volunteer("LEO", "duff", "miguel", "leo.duff@voleur.ytb", "+330069696969"),
+                new Volunteer("leo", "DUFF", "zera", "leo.duff@voleur.ytb", "+330069696969")
+        );
 
         List<Volunteer> expectedVolunteers = new ArrayList<>();
         expectedVolunteers.add(new Volunteer("leo", "duff", "duff;miguel;zera", "leo.duff@voleur.ytb", "0069696969"));
 
-        FieldsSanitizerTesting fieldsSanitizer = new FieldsSanitizerTesting();
-
-        List<Volunteer> resultVolunteers = Cleaner.cleanUp(volunteers, fieldsSanitizer);
-
-        Assertions.assertEquals(expectedVolunteers.toString(), resultVolunteers.toString());
+        whenCleaningVolunteers();
+        thenExpectedListIs(expectedVolunteers);
     }
 
     @Test
     public void shouldRegroupPhones() {
-        List<Volunteer> volunteers = new ArrayList<>();
-        volunteers.add(new Volunteer("leo", "duff", "duff", "leo.duff@voleur.ytb", "+330069696969"));
-        volunteers.add(new Volunteer("LEO", "duff", "duff", "leo.duff@voleur.ytb", "+330069696969"));
-        volunteers.add(new Volunteer("leo", "DUFF", "duff", "leo.duff@voleur.ytb", "+330069696968"));
+        addVolunteers(
+                new Volunteer("leo", "duff", "duff", "leo.duff@voleur.ytb", "+330069696969"),
+                new Volunteer("LEO", "duff", "duff", "leo.duff@voleur.ytb", "+330069696969"),
+                new Volunteer("leo", "DUFF", "duff", "leo.duff@voleur.ytb", "+330069696968")
+        );
 
         List<Volunteer> expectedVolunteers = new ArrayList<>();
         expectedVolunteers.add(new Volunteer("leo", "duff", "duff", "leo.duff@voleur.ytb", "0069696969;0069696968"));
 
-        FieldsSanitizerTesting fieldsSanitizer = new FieldsSanitizerTesting();
-
-        List<Volunteer> resultVolunteers = Cleaner.cleanUp(volunteers, fieldsSanitizer);
-
-        Assertions.assertEquals(expectedVolunteers.toString(), resultVolunteers.toString());
+        whenCleaningVolunteers();
+        thenExpectedListIs(expectedVolunteers);
     }
 
     @Test
     public void shouldRegroupEmptyNickNames() {
-        List<Volunteer> volunteers = new ArrayList<>();
-        volunteers.add(new Volunteer("leo", "duff", "", "leo.duff@voleur.ytb", "+330069696969"));
-        volunteers.add(new Volunteer("LEO", "duff", "miguel", "leo.duff@voleur.ytb", "+330069696969"));
-        volunteers.add(new Volunteer("leo", "DUFF", "zera", "leo.duff@voleur.ytb", "+330069696969"));
+        addVolunteers(
+                new Volunteer("leo", "duff", "", "leo.duff@voleur.ytb", "+330069696969"),
+                new Volunteer("LEO", "duff", "miguel", "leo.duff@voleur.ytb", "+330069696969"),
+                new Volunteer("leo", "DUFF", "zera", "leo.duff@voleur.ytb", "+330069696969")
+        );
 
         List<Volunteer> expectedVolunteers = new ArrayList<>();
         expectedVolunteers.add(new Volunteer("leo", "duff", "miguel;zera", "leo.duff@voleur.ytb", "0069696969"));
 
-        FieldsSanitizerTesting fieldsSanitizer = new FieldsSanitizerTesting();
-
-        List<Volunteer> resultVolunteers = Cleaner.cleanUp(volunteers, fieldsSanitizer);
-
-        Assertions.assertEquals(expectedVolunteers.toString(), resultVolunteers.toString());
+        whenCleaningVolunteers();
+        thenExpectedListIs(expectedVolunteers);
     }
 
     @Test
     public void shouldRegroupPhonesOnNickName() {
-        List<Volunteer> volunteers = new ArrayList<>();
-        volunteers.add(new Volunteer("", "", "roche", "example@voleur.ytb", "+330069696967"));
-        volunteers.add(new Volunteer("", "", "miguel", "example@voleur.ytb", "+330069696969"));
-        volunteers.add(new Volunteer("", "", "roche", "example@voleur.ytb", "+330069696968"));
+        addVolunteers(
+                new Volunteer("", "", "roche", "example@voleur.ytb", "+330069696967"),
+                new Volunteer("", "", "miguel", "example@voleur.ytb", "+330069696969"),
+                new Volunteer("", "", "roche", "example@voleur.ytb", "+330069696968")
+        );
 
         List<Volunteer> expectedVolunteers = new ArrayList<>();
         expectedVolunteers.add(new Volunteer("", "", "roche", "example@voleur.ytb", "0069696967;0069696968"));
         expectedVolunteers.add(new Volunteer("", "", "miguel", "example@voleur.ytb", "0069696969"));
 
-        FieldsSanitizerTesting fieldsSanitizer = new FieldsSanitizerTesting();
-
-        List<Volunteer> resultVolunteers = Cleaner.cleanUp(volunteers, fieldsSanitizer);
-
-        Assertions.assertEquals(expectedVolunteers.toString(), resultVolunteers.toString());
+        whenCleaningVolunteers();
+        thenExpectedListIs(expectedVolunteers);
     }
 
     @Test
     public void shouldRegroupEmailsOnNickName() {
-        List<Volunteer> volunteers = new ArrayList<>();
-        volunteers.add(new Volunteer("", "", "roche", "example@voleur.ytb", "+330069696968"));
-        volunteers.add(new Volunteer("", "", "miguel", "example@voleur.ytb", "+330069696969"));
-        volunteers.add(new Volunteer("", "", "roche", "example@voleur.twitch", "+330069696968"));
+        addVolunteers(
+                new Volunteer("", "", "roche", "example@voleur.ytb", "+330069696968"),
+                new Volunteer("", "", "miguel", "example@voleur.ytb", "+330069696969"),
+                new Volunteer("", "", "roche", "example@voleur.twitch", "+330069696968")
+        );
 
         List<Volunteer> expectedVolunteers = new ArrayList<>();
         expectedVolunteers.add(new Volunteer("", "", "roche", "example@voleur.ytb;example@voleur.twitch", "0069696968"));
         expectedVolunteers.add(new Volunteer("", "", "miguel", "example@voleur.ytb", "0069696969"));
 
-        FieldsSanitizerTesting fieldsSanitizer = new FieldsSanitizerTesting();
-
-        List<Volunteer> resultVolunteers = Cleaner.cleanUp(volunteers, fieldsSanitizer);
-
-        Assertions.assertEquals(expectedVolunteers.toString(), resultVolunteers.toString());
+        whenCleaningVolunteers();
+        thenExpectedListIs(expectedVolunteers);
     }
+
+    private void addVolunteers(Volunteer... volunteers) {
+        Collections.addAll(this.volunteers, volunteers);
+    }
+
+    private void whenCleaningVolunteers() {
+        FieldsSanitizerTesting fieldsSanitizer = new FieldsSanitizerTesting();
+        cleared = Cleaner.cleanUp(volunteers, fieldsSanitizer);
+    }
+
+    private void thenExpectedListIs(List<Volunteer> expected) {
+        Assertions.assertEquals(expected.toString(), cleared.toString());
+    }
+
+    private final List<Volunteer> volunteers = new ArrayList<>();
+    private List<Volunteer> cleared = new ArrayList<>();
 
 }
