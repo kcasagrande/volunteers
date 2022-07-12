2a431f8be2952b24ac4ccb18d54f765c77c0ef42
Robin OGER <oger.robin@gmail.com>
2022-07-12 16:29:29 +0200

    fix: email spaces

--- src/main/java/org/example/volunteers/Cleaner.java
+++ src/main/java/org/example/volunteers/Cleaner.java
@@ -15,6 +15,43 @@ public class Cleaner {
         volunteers = Duplicate.removeDuplicateFirstNameLastNamePseudoMailPhone(volunteers);
         return new ArrayList<Volunteer>(volunteers);
     }
+    public static Boolean isValidEmail(String email) {
+        return Pattern.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", email);
+    }
+    public static String cleanEmailAddress(String email) {
+        email = email.toLowerCase();
+        email = email.replaceAll("é", "e");
+        email = email.replaceAll("è", "e");
+        email = email.replaceAll("ê", "e");
+        email = email.replaceAll("œ", "oe");
+        email = email.replaceAll("ë", "e");
+        email = email.replaceAll("à", "a");
+        email = email.replaceAll("ä", "a");
+        email = email.replaceAll("â", "a");
+        email = email.replaceAll("î", "i");
+        email = email.replaceAll("ï", "i");
+        email = email.replaceAll("ì", "i");
+        email = email.replaceAll("û", "u");
+        email = email.replaceAll("ù", "u");
+        email = email.replaceAll("ü", "u");
+        email = email.replaceAll("ô", "o");
+        email = email.replaceAll("ò", "o");
+        email = email.replaceAll("ö", "o");
+        email = email.replaceAll(" ", "");
+        return email;
+    }
+    public static List<Volunteer> cleanupMailAddresses(List<Volunteer> volunteers) {
+        List<Volunteer> cleanedVolunteers = new ArrayList<>();
+        for(Volunteer volunteer: volunteers) {
+            String cleanEmail = "";
+            String sanitizedEmail = cleanEmailAddress(volunteer.eMail);
+            if (isValidEmail(sanitizedEmail)) {
+                cleanEmail = sanitizedEmail;
+            }
+            cleanedVolunteers.add(new Volunteer(volunteer.firstName, volunteer.lastName, volunteer.nickName, cleanEmail, volunteer.phone));
+        }
+        return cleanedVolunteers;
+    }
 
     public static List<Volunteer> cleanUpUniqueContact(List<Volunteer> volunteers) {
         volunteers = removeAccents(volunteers);
