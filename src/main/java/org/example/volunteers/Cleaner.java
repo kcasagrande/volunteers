7ac6098a683a6734bf64aa5f8fb91a4fae2a2f43
arty3p <gsanchez.dev@gmail.com>
2022-07-12 10:46:58 +0200

    feat (duplicates) : remove unused function

--- src/main/java/org/example/volunteers/Cleaner.java
+++ src/main/java/org/example/volunteers/Cleaner.java
@@ -95,35 +95,4 @@ public class Cleaner {
             return "";
         }
     }
-
-    public static List<Duplicate> extractDuplicated (List<Volunteer> volunteers) {
-        List<Duplicate> duplicatedVolunteers = new ArrayList<>();
-
-        for (Volunteer v : volunteers) {
-            // System.out.println(v.getFullName());
-            String phone = v.phone.toLowerCase(Locale.ROOT).trim();
-            String email = v.eMail.toLowerCase(Locale.ROOT).trim();
-            String firstName = v.firstName.toLowerCase(Locale.ROOT).trim();
-            String lastName = v.lastName.toLowerCase(Locale.ROOT).trim();
-            String nickName = v.nickName.toLowerCase(Locale.ROOT).trim();
-
-            List<Volunteer> duplicates = volunteers.stream()
-                    .filter((volunteer) -> !volunteer.id.equals(v.id))
-                    .filter((volunteer) -> (
-                            volunteer.eMail.toLowerCase(Locale.ROOT).trim().equals(email)
-                                    || volunteer.phone.toLowerCase(Locale.ROOT).trim().equals(phone))
-                            && (volunteer.firstName.toLowerCase(Locale.ROOT).trim().equals(firstName)
-                                || volunteer.lastName.toLowerCase(Locale.ROOT).trim().equals(lastName))
-                    )
-                    .filter((volunteer) -> duplicatedVolunteers.stream().noneMatch(vol -> vol.volunteerId.equals(volunteer.id)))
-                    .collect(Collectors.toList());
-
-            if (duplicates.size() > 0) {
-                Duplicate duplicate = new Duplicate(v.id, duplicates, duplicates.size());
-                duplicatedVolunteers.add(duplicate);
-            }
-        }
-
-        return duplicatedVolunteers;
-    }
 }
