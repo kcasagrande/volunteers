f8328f478a534634217ba4b8d42b1f08d9f91b41
Nicolas Barbosa <nicolas.barbosa@orange.com>
2022-07-12 16:28:49 +0200

    Refactor tests into multiple files

---
+++ src/test/java/org/example/volunteers/NamesCleanerTest.java
@@ -0,0 +1,76 @@
+package org.example.volunteers;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NamesCleanerTest {
+
+    @Test
+    public void doesNothingIfNicknameIsEmpty() {
+        givenVolunteer("Hugo", "Bordais", "", "hugo.bordais@cuck.com", "+33070000000");
+        whenCleaningUpVolunteers();
+        thenNickNameIs("");
+    }
+
+    @Test
+    public void firstNameIsFormatted() {
+        givenVolunteer("hugo", "Bordais", "", "hugo.bordais@cuck.com", "0700000000");
+        whenCleaningUpVolunteers();
+        thenFirstNameIs("Hugo");
+    }
+
+    @Test
+    public void lastNameIsFormatted() {
+        givenVolunteer("Hugo", "bordais", "", "hugo.bordais@cuck.com", "0700000000");
+        whenCleaningUpVolunteers();
+        thenLastNameIs("Bordais");
+    }
+
+    @Test
+    public void lowercaseFirstNameWhenIsFullUppercase() {
+        givenVolunteer("HUGO", "Bordais", "", "hugo.bordais@cuck.com", "0700000000");
+        whenCleaningUpVolunteers();
+        thenFirstNameIs("Hugo");
+    }
+
+    @Test
+    public void lowercaseFirstNameWhenIsFullUppercaseWithDash() {
+        givenVolunteer("JEAN-JACQUES", "Bordais", "", "hugo.bordais@cuck.com", "0700000000");
+        whenCleaningUpVolunteers();
+        thenFirstNameIs("Jean-Jacques");
+    }
+
+    @Test
+    public void lowercaseFirstNameWhenSomeLettersAreUppercase() {
+        givenVolunteer("JeAn-JaCqUeS", "Bordais", "", "hugo.bordais@cuck.com", "0700000000");
+        whenCleaningUpVolunteers();
+        thenFirstNameIs("Jean-Jacques");
+    }
+
+    private void givenVolunteer(String firstName, String lastName, String nickname, String eMail, String phone) {
+        volunteers.add(new Volunteer(firstName, lastName, nickname, eMail, phone));
+    }
+
+    private void whenCleaningUpVolunteers() {
+        cleanedVolunteers = Cleaner.cleanUp(volunteers);
+    }
+
+    private void thenFirstNameIs(String firstName) {
+        assertEquals(firstName, cleanedVolunteers.get(0).firstName);
+    }
+
+    private void thenLastNameIs(String lastName) {
+        assertEquals(lastName, cleanedVolunteers.get(0).lastName);
+    }
+
+    private void thenNickNameIs(String nickname) {
+        assertEquals(nickname, cleanedVolunteers.get(0).nickName);
+    }
+
+    private List<Volunteer> volunteers = new ArrayList<Volunteer>();
+    private List<Volunteer> cleanedVolunteers = new ArrayList<Volunteer>();
+}
---
+++ src/test/java/org/example/volunteers/PhoneCleanerTest.java
@@ -0,0 +1,66 @@
+package org.example.volunteers;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PhoneCleanerTest {
+
+    @Test
+    public void transformPhoneNumberInFrenchFormat() {
+        givenVolunteer("test", "test", "test", "test@test.test", "00-55-51-64-64");
+        whenCleaningUpVolunteers();
+        thenPhoneIs("+33055516464");
+    }
+
+    @Test
+    public void transformPhoneNumberWith33AtBeginningInFrenchFormat() {
+        givenVolunteer("test", "test", "test", "test@test.test", "+33(0)0-55-55-78-08");
+        whenCleaningUpVolunteers();
+        thenPhoneIs("+33055557808");
+    }
+
+    @Test
+    public void transformPhoneNumberWith33InsideInFrenchFormat() {
+        givenVolunteer("test", "test", "test", "test@test.test", "+33(0)0-55-33-78-08");
+        whenCleaningUpVolunteers();
+        thenPhoneIs("+33055337808");
+    }
+
+    @Test
+    public void transformPhoneNumberWith33InsideInFrenchFormat2() {
+        givenVolunteer("test", "test", "test", "test@test.test", "(0)0-55-33-78-08");
+        whenCleaningUpVolunteers();
+        thenPhoneIs("+33055337808");
+    }
+
+    @Test
+    public void transformPhoneNumberWithDotsInsideFrenchFormat() {
+        givenVolunteer("test", "test", "test", "test@test.test", "+330.45.55.69.06");
+        whenCleaningUpVolunteers();
+        thenPhoneIs("+33045556906");
+    }
+
+    @Test
+    public void doesNotTransformPhoneNumberWhenEmpty() {
+        givenVolunteer("test", "test", "test", "test@test.test", "");
+        whenCleaningUpVolunteers();
+        thenPhoneIs("");
+    }
+
+    private void givenVolunteer(String firstName, String lastName, String nickname, String eMail, String phone) {
+        volunteers.add(new Volunteer(firstName, lastName, nickname, eMail, phone));
+    }
+    private void whenCleaningUpVolunteers() {
+        cleanedVolunteers = Cleaner.cleanUp(volunteers);
+    }
+    private void thenPhoneIs(String phone) {
+        assertEquals(phone, cleanedVolunteers.get(0).phone);
+    }
+
+    private List<Volunteer> volunteers = new ArrayList<Volunteer>();
+    private List<Volunteer> cleanedVolunteers = new ArrayList<Volunteer>();
+}
