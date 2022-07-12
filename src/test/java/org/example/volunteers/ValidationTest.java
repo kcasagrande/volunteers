613f108973d569c95e521eae1833d27ad822d78d
DrivoireFR <54139583+DrivoireFR@users.noreply.github.com>
2022-07-12 16:26:52 +0200

    remove whitespace

--- src/test/java/org/example/volunteers/ValidationTest.java
+++ src/test/java/org/example/volunteers/ValidationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ValidationTest {
+
     @Test
     public void shouldHaveValidPhoneNumber() {
         Volunteer testVolunteer = new Volunteer("Matisse", "LIVAIN",
